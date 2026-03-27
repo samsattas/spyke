6e894ebb0e59b06ad6001b98b9342fe5680640c3
Samuel Satizabal <samuelsatizabal@hotmail.com>
2026-03-27 00:53:08 -0500

    Configure GitHub Pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/spyke">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/setup" element={<Setup />} />
